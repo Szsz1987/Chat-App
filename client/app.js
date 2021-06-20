e0e388a5ca0a6674d27c159ae3878b8a5eff0ef4
Szymon <sszulinski@gmail.com>
2021-06-20 14:06:36 +0200

    Task 28.3 - add new functionality for sockets

--- client/app.js
+++ client/app.js
@@ -2,6 +2,8 @@
 
 const socket = io();
 socket.on('message', ({author, content}) => addMessage(author, content));
+socket.on('newUser', ({ author, content }) => addOrRemoveUserMessage(author, content));
+socket.on('userHasLeft', ({ author, content }) => addOrRemoveUserMessage(author, content));
 
 const references = {
   loginForm: document.getElementById('welcome-form'),
@@ -20,31 +22,47 @@ const login = function(event) {
     userName = references.userNameInput.value;
     references.messagesSection.classList.add('show');
     references.loginForm.classList.remove('show');
+    socket.emit('login', {name: userName, id: socket.id });
   } else window.alert('Write your login!');
 };
 
 function addMessage(author, content) {
-    const message = document.createElement('li');
-    message.classList.add('message');
-    message.classList.add('message--received');
-    if(author === userName) message.classList.add('message--self');
-    message.innerHTML = `
-      <h3 class="message__author">${userName === author ? 'You' : author }</h3>
-      <div class="message__content">
-        ${content}
-      </div>
-    `;
-    references.messagesList.appendChild(message);
-  }
-  
-  const sendMessage = function(event) {
-    event.preventDefault();
-    if(references.messageContentInput.value) {
-      addMessage(userName, references.messageContentInput.value);
-      socket.emit('message', { author: userName, content: references.messageContentInput.value });
-      references.messageContentInput.value = '';
-    } else window.alert('You have to type your message');
-  }
-  
-  references.loginForm.addEventListener('submit', login);
-  references.addMessageForm.addEventListener('submit', sendMessage); 
+  const message = document.createElement('li');
+  message.classList.add('message');
+  message.classList.add('message--received');
+  if(author === userName) message.classList.add('message--self');
+  message.innerHTML = `
+    <h3 class="message__author">${userName === author ? 'You' : author }</h3>
+    <div class="message__content">
+      ${content}
+    </div>
+  `;
+  references.messagesList.appendChild(message);
+}
+
+function addOrRemoveUserMessage(author, content) {
+  const message = document.createElement('li');
+  message.classList.add('message');
+  message.classList.add('message--received');
+  message.classList.add('message--self');
+  message.classList.add('message--changeUser');
+  message.innerHTML = `
+    <h3 class="message__author">${ author }</h3>
+    <div class="message__content">
+      ${ content }
+    </div>
+  `;
+  references.messagesList.appendChild(message);
+}
+
+const sendMessage = function(event) {
+  event.preventDefault();
+  if(references.messageContentInput.value) {
+    addMessage(userName, references.messageContentInput.value);
+    socket.emit('message', { author: userName, content: references.messageContentInput.value });
+    references.messageContentInput.value = '';
+  } else window.alert('You have to type your message');
+}
+
+references.loginForm.addEventListener('submit', login);
+references.addMessageForm.addEventListener('submit', sendMessage);
